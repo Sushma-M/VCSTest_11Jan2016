07e76a3a113a2b674e3e9c18e065132eed9f73f6
Sushma Maddirala <sushma.maddirala@wavemaker.com>
2016-01-11 12:44:30 +0000

    New files added - [] Files modified - [Main.variables.json,Main.html,Main.js,page.min.html,app.variables.json] Refactored files - [] Fixed bugs - [] Removed files - []

--- src/main/webapp/pages/Main/Main.js
+++ src/main/webapp/pages/Main/Main.js
@@ -19,3 +19,17 @@ Application.$controller("MainPageController", ["$scope", function ($scope) {
     };
 
 }]);
+
+Application.$controller("VacationGrid1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
+
+Application.$controller("VacationLiveform1Controller", ["$scope",
+	function($scope) {
+		"use strict";
+		$scope.ctrlScope = $scope;
+	}
+]);
